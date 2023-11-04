778ddd25c430ee57a96cebbc7d1de234b93d3e0a
Adrián Arribas Mateo <100200834+adrianchu539@users.noreply.github.com>
2023-11-04 20:17:27 +0100

    Añadir comentarios a ficheros antiguos y componente ruleta para sorteos

--- components/login.js
+++ components/login.js
@@ -1,71 +1,102 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { auth } from '@/firebase';
-import {GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useRouter } from "next/router"; 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { auth } from "@/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useRouter } from "next/router";
 
 function Login() {
-const provider = new GoogleAuthProvider();
-const router = useRouter();
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
-        .then((result) => {
+	// Uso del provider de autentificación de Google
+	const provider = new GoogleAuthProvider();
 
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
+	// Uso del componente router para redirección entre páginas
+	const router = useRouter();
 
-          const user = result.user;
-          
-          // Extraemos los datos que pueden ser útiles
-          const nombre = result.user.displayName;
-          const email = result.user.email;
-          const foto = result.user.photoURL;
+	// Función para gestionar el inicio de sesión con Google
+	const signInWithGoogle = () => {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				// Declaración de credenciales y token de acceso. NO SÉ SI SON NECESARIOS (!!)
+				const credential =
+					GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
 
-          //Guardamos los datos en la bd del navegador para un rápido acceso
-          localStorage.setItem("nombre", nombre);
-          localStorage.setItem("email", email);
-          localStorage.setItem("foto", foto);
+				// Declaración del usuario. NO SÉ SI ES NECESARIO (!!)
+				const user = result.user;
 
-          router.push("/eventos");
-      })
-        .catch((error) => {
-          console.log(error);
-        })
-    }
+				// Extraemos los datos que pueden ser útiles. NO SÉ SI ES NECESARIO (!!)
+				const nombre = result.user.displayName;
+				const email = result.user.email;
+				const foto = result.user.photoURL;
 
-    const redirigirSignUp = () => {
-      router.push("/signup");
-    }
-  var sectionStyle = {
-    backgroundImage: `url(https://www.lafactoriadelshow.com/blog/wp-content/uploads/2023/03/ideas-fiestas-tematicas.jpeg)`
-  }
+				// Guardamos los datos en la bd del navegador para un rápido acceso. NO SÉ SI ES NECESARIOS (!!)
+				localStorage.setItem("nombre", nombre);
+				localStorage.setItem("email", email);
+				localStorage.setItem("foto", foto);
 
-  return (
-    <div className='login d-flex justify-content-center align-items-center 100-w vh-100' style={sectionStyle}>
-      <div className='form_container w-70 p-5 rounded border border-dark bg-white'>
-          <Form>
-            <h3>Iniciar sesión</h3>
-            <div className='mb-2'>
-              <Form.Label>Correo electrónico</Form.Label>
-              <input type="email" placeholder='Introduce correo' className='form-control'/>
-            </div>
-            <div className='mb-2'>
-              <Form.Label>Contraseña</Form.Label>
-              <input type="password" placeholder='Introduce contraseña' className='form-control'/>
-            </div>
-          <div className='d-grid'>
-            <Button>Iniciar sesión</Button>
-          </div>
-          <div className='mt-2 d-grid'>
-            <Button onClick={signInWithGoogle}>Iniciar sesión con Google</Button>
-          </div>
-          <div className='mt-2 d-grid'>
-            <Button onClick={redirigirSignUp}>¿No tienes cuenta?</Button>
-          </div>
-        </Form>
-      </div>
-    </div>
-  );
+				// Redireccionamos a eventos tras un inicio de sesión válido
+				router.push("/eventos");
+			})
+			.catch((error) => {
+				// Hacemos log del error en caso de que haya ocurrido algún problema
+				console.log(error);
+			});
+	};
+
+	// Función utilizada para redirigir a la pantalla de creación de cuenta
+	const redirigirSignUp = () => {
+		router.push("/signup");
+	};
+
+	// Imagen de fondo utilizada en las pantallas de login y signup
+	var sectionStyle = {
+		backgroundImage: `url(https://www.lafactoriadelshow.com/blog/wp-content/uploads/2023/03/ideas-fiestas-tematicas.jpeg)`,
+	};
+
+	return (
+		<div
+			className="login d-flex justify-content-center align-items-center 100-w vh-100"
+			style={sectionStyle}
+		>
+			{/* Tal vez aumentar anchura del div */}
+			<div className="form_container w-70 p-5 rounded border border-dark bg-white">
+				<Form>
+					<h3>Iniciar sesión</h3>
+					<div className="mb-2">
+						<Form.Label>Correo electrónico</Form.Label>
+						<input
+							type="email"
+							placeholder="Introduce correo"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-2">
+						<Form.Label>Contraseña</Form.Label>
+						<input
+							type="password"
+							placeholder="Introduce contraseña"
+							className="form-control"
+						/>
+					</div>
+					{/* Tal vez incrementar margin-top */}
+					<div className="d-grid">
+						{/* Por completar para inicio de sesión con correo y contraseña... */}
+						<Button>Iniciar sesión</Button>
+					</div>
+					<div className="mt-2 d-grid">
+						{/* Intentar cambiar formato botón Google */}
+						<Button onClick={signInWithGoogle}>
+							Iniciar sesión con Google
+						</Button>
+					</div>
+					<div className="mt-2 d-grid">
+						<Button onClick={redirigirSignUp}>
+							¿No tienes cuenta?
+						</Button>
+					</div>
+				</Form>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
---
+++ components/ruletaNombres/listaNombres.js
@@ -0,0 +1,10 @@
+// Lista con los nombres de los invitados de prueba.
+// PROXIMAMENTE SE USARÁN LOS INVITADOS DEL EVENTO Y NO SE NECESITARÁ ESTE FICHERO (!!)
+export const userNames = [
+	"Nombre-1",
+	"Nombre-2",
+	"Nombre-3",
+	"Nombre-4",
+	"Nombre-5",
+	"Nombre-6",
+];
