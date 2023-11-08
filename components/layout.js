fab76c91dc1b7e7495c1ee26a04b0d2052055168
Adrián Arribas Mateo <100200834+adrianchu539@users.noreply.github.com>
2023-11-08 11:29:41 +0100

    Arreglar login cuando token invalido

--- components/layout.js
+++ components/layout.js
@@ -4,37 +4,39 @@ import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
-    // Uso del componente router para redirección entre páginas
-    const router = useRouter();
+	// Uso del componente router para redirección entre páginas
+	const router = useRouter();
 
-    // Función para gestionar el cierre de sesión del usuario
-    const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                // Redirecciona al usuario a la pantalla inicial
-                localStorage.clear();
-                router.push("/");
-                console.log("Signed out successfully");
-            })
-            .catch((error) => {
-                // Hacemos log del error en caso de que haya ocurrido algún problema
-                console.log(error);
-            });
-    };
-    return (
-        <>
-            {/* Barra de navegación */}
-            <nav className="navbar navbar-dark bg-dark">
-                <div className="container text-center">
-                    <span className="navbar-brand mx-auto">InvitApp</span>
-                    {/* Se ha incluido botón para gestionar el cierre de sesión */}
-                    <Button onClick={() => handleLogout()}>Logout</Button>
-                </div>
-            </nav>
+	// Función para gestionar el cierre de sesión del usuario
+	const handleLogout = () => {
+		signOut(auth)
+			.then(() => {
+				// Redirecciona al usuario a la pantalla inicial
+				localStorage.clear();
+				router.push("/");
+				console.log("Signed out successfully");
+			})
+			.catch((error) => {
+				// Hacemos log del error en caso de que haya ocurrido algún problema
+				console.log(error);
+			});
+	};
+	return (
+		<>
+			{/* Barra de navegación */}
+			<nav className="navbar navbar-dark bg-dark">
+				<div className="container text-center">
+					<span className="navbar-brand mx-auto">InvitApp</span>
+					{/* Se ha incluido botón para gestionar el cierre de sesión */}
+					<Button onClick={() => handleLogout()}>
+						Cerrar sesión
+					</Button>
+				</div>
+			</nav>
 
-            {children}
-        </>
-    );
+			{children}
+		</>
+	);
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -10,37 +10,37 @@ import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { auth } from "@/firebase";
 
 export default function Home() {
-    const [cargado, setCargado] = useState(false);
-    const [idToken, setIdToken] = useState(null);
-    useEffect(() => {
-        // Perform localStorage action
-        setIdToken(localStorage.getItem("idToken"));
-        setCargado(true);
-    }, []);
-    // Uso del componente router para redirección entre páginas
-    const router = useRouter();
-    // Si no hay nadie en la sesión actual se mostrará la página de Login
-    if (cargado) {
-        if (idToken === null) {
-            return <Login></Login>;
-        } else {
-            const credential = GoogleAuthProvider.credential(idToken);
-            // Comprobar que la sesion es valida
-            signInWithCredential(auth, credential)
-                .then(() => {
-                    router.push("/eventos");
-                })
-                .catch(() => {
-                    return <Login></Login>;
-                });
-        }
-    }
-    // // Si hay alguien en la sesión actual se redirigirá directamente a eventos
-    // if (user) {
-    //     router.push("/eventos");
-    // }
-    // // Si no hay nadie en la sesión actual se mostrará la página de Login
-    // else {
-    //     return <Login></Login>;
-    // }
+	const [cargado, setCargado] = useState(false);
+	const [idToken, setIdToken] = useState(null);
+	useEffect(() => {
+		// Perform localStorage action
+		setIdToken(localStorage.getItem("idToken"));
+		setCargado(true);
+	}, []);
+	// Uso del componente router para redirección entre páginas
+	const router = useRouter();
+	// Si no hay nadie en la sesión actual se mostrará la página de Login
+	if (cargado) {
+		if (idToken === null) {
+			router.push("/login");
+		} else {
+			const credential = GoogleAuthProvider.credential(idToken);
+			// Comprobar que la sesion es valida
+			signInWithCredential(auth, credential)
+				.then(() => {
+					router.push("/eventos");
+				})
+				.catch((error) => {
+					router.push("/login");
+				});
+		}
+	}
+	// // Si hay alguien en la sesión actual se redirigirá directamente a eventos
+	// if (user) {
+	//     router.push("/eventos");
+	// }
+	// // Si no hay nadie en la sesión actual se mostrará la página de Login
+	// else {
+	//     return <Login></Login>;
+	// }
 }
